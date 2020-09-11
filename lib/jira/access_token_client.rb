57c5b4c87f47af38a5e20263f3aec385c02b9604
jhendley25 <jake.hendley25@gmail.com>
2020-09-11 12:24:58 -0400

    finally found the issue with all the url building stuff

--- lib/jira/access_token_client.rb
+++ lib/jira/access_token_client.rb
@@ -16,7 +16,11 @@ module JIRA
 
     def make_request(http_method, url, body = '', headers = {})
       # When a proxy is enabled, Net::HTTP expects that the request path omits the domain name
-      path = request_path(url)
+      # this whole freakin gem is convoluted.
+      # the process to even build a url is abstracted to a dozen different condition and wrapped in arbitrary methods.
+      # overriding most of it for now and just building a proper url. bah
+      path = @options[:site] + request_path(url)
+      puts 'path in make request', path
       request = Net::HTTP.const_get(http_method.to_s.capitalize).new(path, headers.merge(@headers))
       request.body = body unless body.nil?
 
@@ -56,6 +60,7 @@ module JIRA
 
     def execute_request(request)
       response = access_token_http_conn.request(request)
+
       @authenticated = response.is_a? Net::HTTPOK
 
       response
@@ -64,6 +69,7 @@ module JIRA
     def request_path(url)
       parsed_uri = URI(url)
 
+      puts 'uri in request_path:', parsed_uri
       return url unless parsed_uri.is_a?(URI::HTTP)
 
       parsed_uri.request_uri
